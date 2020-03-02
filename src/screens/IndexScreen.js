a1e2e28deaf4d1328fa01049f6c3314110dcbfb8
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-02 10:49:27 -0800

    inital commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+
+const IndexScreen = () => {
+ const blogPosts = useContext(BlogContext);
+ 
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <FlatList 
+        data={blogPosts}
+        keyExtractor={key => key.title}
+        renderItem={({item}) => {
+        return <Text>{item.title}</Text>
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default IndexScreen;
