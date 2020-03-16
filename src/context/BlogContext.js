a154b8a4c266f41ff1be947f9f3e9539efa1aa04
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-16 12:33:16 -0700

    working on making a reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState  } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { title: 'Blog Post #1' },
-    { title: 'Blog Post #2' },
-]
+const [blogPosts, setBlogPosts] = useState([])
 
+const addBlogPost = () => {
+  setBlogPosts([...blogPosts, {title: `BlogPost #${blogPosts.length + 1}`}]);
+};
 
   return (
-    <BlogContext.Provider value={blogPosts}>
+    <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
       {children}
     </BlogContext.Provider>
   );
