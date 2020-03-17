07420e08212d02cb6b15f09cbc1c4114ead16bb3
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-17 16:13:08 -0700

    commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,4 @@
-import React, { useReducer  } from "react";
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch(action.type){
@@ -11,18 +9,10 @@ const blogReducer = (state, action) => {
   }
 };
 
-export const BlogProvider = ({ children }) => {
-const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-const addBlogPost = () => {
-  dispatch({type: 'add_blogPost'});
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({type: 'add_blogPost'});
+  }
 }
 
-  return (
-    <BlogContext.Provider value={{data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
-};
-
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, [])
